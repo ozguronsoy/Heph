a0131b85b6a0ccd8bcbabec5d20873700b88b112
Özgür <ozguronsoy@gmail.com>
2025-02-24 23:44:16 +0300

    android bug fix

--- src/UUID.cpp
+++ src/UUID.cpp
@@ -144,7 +144,13 @@ namespace Heph
 
     UUID::operator std::string() const
     {
-        return std::format("{}", *this);
+        return std::format("{:02X}{:02X}{:02X}{:02X}-{:02X}{:02X}-{:02X}{:02X}-{:02X}{:02X}-{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}",
+            this->data[0], this->data[1], this->data[2], this->data[3],
+            this->data[4], this->data[5],
+            this->data[6], this->data[7],
+            this->data[8], this->data[9],
+            this->data[10], this->data[11], this->data[12], this->data[13], this->data[14], this->data[15]
+        );
     }
 
     UUID::operator std::wstring() const
