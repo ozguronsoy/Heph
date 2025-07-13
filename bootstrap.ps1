0f69c04514bdc21207b1b7d307070caee6c868d6
Özgür <ozguronsoy@gmail.com>
2025-07-13 21:23:07 +0300

    added bootstrap powershell script

---
+++ bootstrap.ps1
@@ -0,0 +1,373 @@
+# ----------------------------------------
+# PARAMS
+# ----------------------------------------
+
+param (
+    [string]$OutputDir = "build",
+    [string]$CompilerDir = "",
+    
+    [switch]$gcc,
+    [switch]$clang,
+    [switch]$msvc,
+    [string]$Generator = "",
+    
+    [switch]$Debug,
+    [switch]$Release,
+    [switch]$Rebuild, # removes the output directory to do a clean build 
+    [switch]$Clean, # removes the output directory and stops the configuration
+    
+    [switch]$CleanCache, # removes the cache file
+    [switch]$NoCache, # does not use cache for current configuration but does not delete the existsing cache neither
+    
+    [switch]$BuildTests,
+    [switch]$BuildDocs,
+    
+    [switch]$Shared,
+    [switch]$Static,
+
+    [switch]$Verbose
+)
+
+[string]$CacheFilePath = ".configure_cache"
+
+[string]$CMakeOptions = ""
+[string]$BuildType = ""
+
+[string]$Compiler = ""
+[string]$C_Compiler = ""
+[string]$Cpp_Compiler = ""
+
+[string[]]$Compilers = @(
+                           "gcc",
+                           "msvc",
+                           "clang"
+                    )
+
+
+[string[]]$Generators = @(
+                            "Ninja",
+                            "Unix Makefiles",
+                            "Visual Studio 17 2022",
+                            "MinGW Makefiles"
+                    )
+
+[ConsoleColor]$InfoColor = "Gray"
+[ConsoleColor]$SuccessColor = "Green"
+[ConsoleColor]$WarningColor = "Yellow"
+[ConsoleColor]$ErrorColor = "Red"
+
+if ($Debug -and $Release)
+{
+    Write-Host "[Heph] You cannot specify more than one build type." -ForegroundColor $ErrorColor
+    exit 1
+}
+
+if ((@($gcc, $msvc, $clang) | Where-Object { $_ }).Count -gt 1)
+{
+    Write-Host "[Heph] You cannot specify more than one compiler option." -ForegroundColor $ErrorColor
+    exit 1
+}
+
+if ($Verbose)
+{
+    $VerbosePreference = "Continue"
+}
+
+# ----------------------------------------
+# END PARAMS
+# ----------------------------------------
+
+# ----------------------------------------
+# CLEAN
+# ----------------------------------------
+
+if ($Clean -or $Rebuild)
+{
+    Write-Host "[Heph] Cleaning." -ForegroundColor $InfoColor
+    
+    if (Test-Path $OutputDir)
+    {
+        Remove-Item -Path $OutputDir -Recurse -Force
+
+        if (Test-Path $OutputDir)
+        {
+            Write-Host "[Heph] Failed to remove the output directory, continuing the build." -ForegroundColor $WarningColor
+        }
+    }
+}
+
+if ($CleanCache -and (Test-Path $CacheFilePath))
+{
+    Write-Host "[Heph] Removing the cache file." -ForegroundColor $InfoColor
+    Remove-Item $CacheFilePath -Force
+
+    if (Test-Path $CacheFilePath)
+    {
+        Write-Host "[Heph] Failed to remove the cache file." -ForegroundColor $ErrorColor
+        exit 1
+    }
+}
+
+if ($Clean)
+{
+    exit 0
+}
+
+# ----------------------------------------
+# END CLEAN
+# ----------------------------------------
+
+# ----------------------------------------
+# RESOTRE CACHE
+# ----------------------------------------
+
+if (-not $NoCache -and -not $CleanCache)
+{
+    Write-Host "[Heph] Restoring options from the cache file." -ForegroundColor $InfoColor
+
+    if (Test-Path $CacheFilePath)
+    {
+        $Cache = Get-Content -Path $CacheFilePath
+
+        $CacheMap = @{}
+
+        foreach ($Line in $Cache) 
+        {
+            if ($Line -match "^\s*#") { continue }  # skip comments
+            if ($Line -match "^\s*$") { continue }  # skip blank lines
+        
+            $Key, $Value = $Line -split '=', 2
+            $CacheMap[$Key.Trim()] = $Value.Trim()
+        }
+
+        if ($CacheMap.ContainsKey("OutputDir") -and -not $PSBoundParameters.ContainsKey("OutputDir"))
+        {
+            $OutputDir = $CacheMap["OutputDir"]
+        }
+
+        if ($CacheMap.ContainsKey("CompilerDir") -and -not $PSBoundParameters.ContainsKey("CompilerDir"))
+        {
+            $CompilerDir = $CacheMap["CompilerDir"]
+        }
+
+        if ($CacheMap.ContainsKey("Compiler") -and -not $gcc -and -not $clang -and -not $msvc)
+        {
+            $Compiler = $CacheMap["Compiler"]
+        }
+
+        if ($CacheMap.ContainsKey("Generator") -and -not $PSBoundParameters.ContainsKey("Generator"))
+        {
+            $Generator = $CacheMap["Generator"]
+        }
+
+        if ($CacheMap.ContainsKey("BuildType"))
+        {
+            $BuildType = $CacheMap["BuildType"]
+
+            if (($BuildType -eq "Release" -and $PSBoundParameters.ContainsKey("Debug")) -or ($BuildType -eq "Debug" -and $PSBoundParameters.ContainsKey("Release")))
+            {
+                $Rebuild = $true
+            }
+        }
+
+        if ($CacheMap.ContainsKey("BuildTests") -and -not $PSBoundParameters.ContainsKey("BuildTests"))
+        {
+            $BuildTests = if ($CacheMap["BuildTests"] -eq "true") { $true } else { $false }
+        }
+
+        if ($CacheMap.ContainsKey("BuildDocs") -and -not $PSBoundParameters.ContainsKey("BuildDocs"))
+        {
+            $BuildDocs = if ($CacheMap["BuildDocs"] -eq "true") { $true } else { $false }
+        }
+
+        if ($CacheMap.ContainsKey("Static") -and -not $PSBoundParameters.ContainsKey("Static"))
+        {
+            $Static = if ($CacheMap["Static"] -eq "true") { $true } else { $false }
+        }
+
+        if ($CacheMap.ContainsKey("Shared") -and -not $PSBoundParameters.ContainsKey("Shared"))
+        {
+            $Shared = if ($CacheMap["Shared"] -eq "true") { $true } else { $false }
+        }
+    }
+}
+
+# ----------------------------------------
+# END RESOTRE CACHE
+# ----------------------------------------
+
+# ----------------------------------------
+# CREATE DIRECTORIES
+# ----------------------------------------
+
+Write-Host "[Heph] Building." -ForegroundColor $InfoColor
+Write-Host "[Heph] Checking for the output directory." -ForegroundColor $InfoColor
+
+
+if (-not (Test-Path $OutputDir))
+{
+    Write-Host "[Heph] Creating the output directory." -ForegroundColor $InfoColor    
+    New-Item -ItemType Directory -Path $OutputDir | Out-Null
+
+    if (!(Test-Path $OutputDir))
+    {
+        Write-Host "[Heph] Failed to create the output directory." -ForegroundColor $ErrorColor
+        exit 1
+    }
+}
+
+# ----------------------------------------
+# END CREATE DIRECTORIES
+# ----------------------------------------
+
+# ----------------------------------------
+# CREATE CMAKE PARAMS
+# ----------------------------------------
+
+if ($gcc)
+{
+    $Compiler = "gcc"
+    $C_Compiler = if ($CompilerDir -ne "") { Join-Path $CompilerDir "gcc" } else { "gcc" }
+    $Cpp_Compiler = if ($CompilerDir -ne "") { Join-Path $CompilerDir "g++" } else { "g++" }
+
+    if (-not ($Generators -contains $Generator))
+    {
+        $Generator = "Ninja"
+    }
+}
+
+if ($clang)
+{
+    $Compiler = "clang"
+    $C_Compiler = if ($CompilerDir -ne "") { Join-Path $CompilerDir "clang" } else { "clang" }
+    $Cpp_Compiler = if ($CompilerDir -ne "") { Join-Path $CompilerDir "clang++" } else { "clang++" }
+    
+    if (-not ($Generators -contains $Generator))
+    {
+        $Generator = "Ninja"
+    }
+}
+
+if ($msvc)
+{
+    $Compiler = "msvc"
+    $C_Compiler = if ($CompilerDir -ne "") { Join-Path $CompilerDir "cl" } else { "cl" }
+    $Cpp_Compiler = if ($CompilerDir -ne "") { Join-Path $CompilerDir "cl" } else { "cl" }
+    
+    if (-not ($Generators -contains $Generator))
+    {
+        $Generator = "Visual Studio 17 2022"
+    }
+}
+
+if ($Generators -contains $Generator)
+{
+    $CMakeOptions += "-G `"$Generator`" "
+}
+else 
+{
+    $Generator = "default"    
+}
+
+if ($Compilers -contains $Compiler)
+{
+    $CMakeOptions += "-DCMAKE_C_COMPILER=`"$C_Compiler`" -DCMAKE_CXX_COMPILER=`"$Cpp_Compiler`" "
+}
+else 
+{
+    $Compiler = "default"    
+}
+
+if ($BuildType -eq "" -or ($BuildType -eq "Release" -and $Debug -and -not $Release) -or ($BuildType -eq "Debug" -and $Release -and -not $Debug))
+{
+    if ($Release)
+    {
+        $BuildType = "Release"
+    }
+    else 
+    {
+        $BuildType = "Debug"
+    }
+}
+
+if ($BuildTests)
+{
+    $CMakeOptions += "-DHEPH_BUILD_TESTS=ON "
+}
+
+if ($BuildDocs)
+{
+    $CMakeOptions += "-DHEPH_BUILD_DOCS=ON "
+}
+
+if ($Static)
+{
+    $CMakeOptions += "-DHEPH_BUILD_STATIC=ON "
+}
+
+if ($Shared)
+{
+    $CMakeOptions += "-DHEPH_BUILD_SHARED=ON "
+}
+
+$CMakeOptions = $CMakeOptions.Trim()
+
+# ----------------------------------------
+# END CREATE CMAKE PARAMS
+# ----------------------------------------
+
+# ----------------------------------------
+# UPDATE CACHE
+# ----------------------------------------
+
+if (-not $NoCache)
+{
+    Write-Host "[Heph] Updating the cache." -ForegroundColor $InfoColor
+
+    if (-not (Test-Path $CacheFilePath))
+    {
+        New-Item -ItemType File -Path $CacheFilePath -Force | Out-Null
+    }
+
+    if (-not (Test-Path $CacheFilePath))
+    {
+        Write-Host "[Heph] Failed to create the cache file." -ForegroundColor $WarningColor
+    }
+    else 
+    {
+        $Cache = ""
+        $Cache += "OutputDir=$OutputDir`n"
+        $Cache += "CompilerDir=$CompilerDir`n"
+        $Cache += "Compiler=$Compiler`n"
+        $Cache += "Generator=$Generator`n"
+        $Cache += "BuildType=$BuildType`n"
+        $Cache += "BuildTests=$($BuildTests.ToString().ToLower())`n"
+        $Cache += "BuildDocs=$($BuildDocs.ToString().ToLower())`n"
+        $Cache += "Static=$($Static.ToString().ToLower())`n"
+        $Cache += "Shared=$($Shared.ToString().ToLower())`n"
+
+        $Cache | Set-Content -Path $CacheFilePath -Encoding UTF8
+    }
+}
+
+# ----------------------------------------
+# END UPDATE CACHE
+# ----------------------------------------
+
+# ----------------------------------------
+# BUILD
+# ----------------------------------------
+
+Write-Host "[Heph] Configuring cmake." -ForegroundColor $InfoColor
+
+Write-Verbose "Running command: 'cmake -S . -B $OutputDir -DCMAKE_BUILD_TYPE=$BuildType $CMakeOptions'"
+cmake -S . -B "$OutputDir" -DCMAKE_BUILD_TYPE="$BuildType" $($CMakeOptions -split " ")
+
+Write-Verbose "Running command: 'cmake --build $OutputDir --config $BuildType -- -j$([Environment]::ProcessorCount)"
+cmake --build "$OutputDir" --config $BuildType -- "-j$([Environment]::ProcessorCount)"
+
+Write-Host "[Heph] Build files have been written to `"$OutputDir`"" -ForegroundColor $SuccessColor
+
+# ----------------------------------------
+# END BUILD
+# ----------------------------------------
